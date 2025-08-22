214e29b5ad64a58d86aaeb406104322ffb89d92d
Patrick <dragon_dragon_dragon@live.cn>
2025-08-23 04:10:53 +0800

    初始化项目：提交src、package.json等核心文件

---
+++ src/assets/index.js
@@ -0,0 +1,52 @@
+import logo from "./logo.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import click from "./click.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import az from "./tech/az.png";
+import pfizer from "./tech/pfizer.png";
+import pingan from "./tech/pingan.png";
+import ningbo from "./tech/ningbo.png";
+import porsche from "./tech/porsche.png";
+import huawei from "./tech/huawei.png";
+import tencent from "./tech/tencent.png";
+import mercedesbenz from "./tech/mercedesbenz.png";
+
+import vhall from "./company/vhall.png";
+import fengfan from "./company/fengfan.png";
+import vokdams from "./company/vokdams.png";
+import cyts from "./company/cyts.png";
+
+import tencent1 from "./tencent1.png";
+import huawei1 from "./huawei1.png";
+import porsche1 from "./porsche1.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  click,
+  menu,
+  close,
+  az,
+  pfizer,
+  pingan,
+  ningbo,
+  porsche,
+  huawei,
+  tencent,
+  mercedesbenz,
+  vhall,
+  fengfan,
+  vokdams,
+  cyts,
+  porsche1,
+  huawei1,
+  tencent1,
+};
